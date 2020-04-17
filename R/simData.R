afb3537662df7267e67aa0f2673505c94102be80
Víthor Rosa Franco <vithor8@gmail.com>
2020-04-17 14:07:35 -0300

    Small correction to simData

--- R/simData.R
+++ R/simData.R
@@ -4,14 +4,11 @@ simData <- function(n=NULL, v=NULL ,l=NULL, p=1, scaling=1.7,
 
   set.seed(seed)
   if (model == "rasch") {
-    source("simRasch.R")
     Result <- simRasch(n=n, v=v, l=l, seed=seed, sequence=sequence,
                        dist=dist, interaction=interaction, weight=weight)
   } else if (model == "sirm") {
-    source("simSirm.R")
     Result <- simSirm(n=n, v=v, l=l, seed=seed, sequence=sequence, dist=dist)
   } else if (model == "plm") {
-    source("simPLM.R")
     Result <- simPLM(n=n, v=v, l=l, p=p, scaling=scaling,
                      seed=seed, sequence=sequence, dist=dist)
 
